5b2cc26871a43d61a34475a3f075ae19b33a1d1d
Yaser-Al-Hameedi <yaseralhameedi@gmail.com>
2026-04-09 16:26:01 -0400

    Testing

--- src/sensors/real_sensors.py
+++ src/sensors/real_sensors.py
@@ -4,6 +4,7 @@ import time
 try:
     import spidev
     import smbus2
+    import glob
     from w1thermsensor import W1ThermSensor
 except ImportError:
     pass
@@ -27,6 +28,24 @@ class RealMoistureSensor(BaseSensor):
 
 
 class RealTemperatureSensor(BaseSensor):
+    base_dir = '/sys/bus/w1/devices/'
+    device_folder = glob.glob(base_dir + '28-*')[0]
+    device_file = device_folder + '/w1_slave'
+
+    def read():
+        with open(device_file, 'r') as f:
+            lines = f.readlines()
+
+        if lines[0].strip()[-3:] != 'YES':
+            return None
+
+        equals_pos = lines[1].find('t=')
+        if equals_pos != -1:
+            temp_string = lines[1][equals_pos+2:]
+            temp_c = float(temp_string) / 1000.0
+            return temp_c
+    '''
+    
     def __init__(self):
         try:
             self.w1 = W1ThermSensor()
@@ -41,7 +60,7 @@ class RealTemperatureSensor(BaseSensor):
         except Exception:
             return 0.0
 
-
+'''
 class RealLightSensor(BaseSensor):
     def __init__(self, address=0x23):
         try:
