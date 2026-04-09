9e2e35ddde16a3393864582736e68410c35d0224
Yaser-Al-Hameedi <yaseralhameedi@gmail.com>
2026-04-09 17:27:22 -0400

    Remove pump cooldown for testing

--- src/pump/controller.py
+++ src/pump/controller.py
@@ -17,10 +17,6 @@ class PumpController:
             GPIO.setup(self.relay_pin, GPIO.OUT)
     
     def water(self, duration_seconds, trigger, model_confidence=None):
-        if self.last_watered is not None:
-            seconds_since_last = (datetime.now() - self.last_watered).total_seconds()
-            if seconds_since_last < settings.pump_cooldown * 60:
-                return
 
         duration_seconds = min(duration_seconds, settings.pump_max_duration * 60)
 
